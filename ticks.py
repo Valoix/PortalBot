f9cdfeebb7d6c3b225b28d6b2f4006ebdc2b96e6
Valoix <97880029+Valoix@users.noreply.github.com>
2024-01-10 12:53:00 +1100

    Improved time2tick & tick2time
    [+] Added support for hour long runs
    [+] Added a time/tick cap to minimize spam abuse
    [+] Slightly more optimized

--- ticks.py
+++ ticks.py
@@ -2,11 +2,14 @@ import math
 
 def minute_checker(time):
     convertedSeconds = 0.0
-    for i in range(len(time)):
-        if time[i:i+1] == ":":
-              convertedSeconds += abs(int(time[:i]))*60+abs(float(time[i+1:]))
-              return round(float(convertedSeconds)/0.015,3)
-    return round(float(time)/0.015,3)
+    time = time.split(":")
+    timecount = len(time)-1
+    for i in time:
+        convertedSeconds += float(i)*(pow(60,timecount))
+        timecount-=1
+    if convertedSeconds > 86400:
+        return False
+    return round(float(convertedSeconds)/0.015,3)
 
 def second_to_minute(time):
     if time >= 60:
@@ -18,6 +21,12 @@ def second_to_minute(time):
             seconds = f"{seconds}0"
 
         minutes = int(time // 60)
+        if minutes >= 60:
+            hours = minutes // 60
+            minutes = minutes % 60
+            if minutes < 10:
+                minutes = "0"+str(minutes)
+            return f"{hours}:{minutes}:{seconds}"
         return f"{minutes}:{seconds}"
        
     else:
@@ -34,14 +43,17 @@ def second_to_minute(time):
 def time_to_tick(time):
     try:
         ticks = minute_checker(time)
-        if ticks.is_integer():
-            correctTime = second_to_minute(round((ticks)*0.015, 3))
-            return f"Your time is valid, being {int(ticks)} tick{'' if ticks == 1 else 's'} / {correctTime} seconds."
-        
+        if ticks:
+            if ticks.is_integer():
+                correctTime = second_to_minute(round((ticks)*0.015, 3))
+                return f"Your time is valid, being {int(ticks)} tick{'' if ticks == 1 else 's'} / {correctTime} seconds."
+            
+            else:
+                floor_time = second_to_minute(round(math.floor(ticks)*0.015, 3))
+                ceil_time = second_to_minute(round(math.ceil(ticks)*0.015, 3))
+                return f"That time is invalid, did you mean:\n{math.floor(ticks)} tick{'' if math.floor(ticks) == 1 else 's'} / {floor_time} seconds\n{math.ceil(ticks)} tick{'' if math.ceil(ticks) == 1 else 's'} / {ceil_time} seconds."
         else:
-            floor_time = second_to_minute(round(math.floor(ticks)*0.015, 3))
-            ceil_time = second_to_minute(round(math.ceil(ticks)*0.015, 3))
-            return f"That time is invalid, did you mean:\n{math.floor(ticks)} tick{'' if math.floor(ticks) == 1 else 's'} / {floor_time} seconds\n{math.ceil(ticks)} tick{'' if math.ceil(ticks) == 1 else 's'} / {ceil_time} seconds."
+            return "That is over a month long, I no no wanna :("
     except Exception as e:
         print(e)
 
@@ -52,6 +64,10 @@ def tick_to_time(ticks):
         try:
             ticks = int(ticks)
             time = float(ticks) * 0.015
+            if time > 2678400:
+                return "That is over a month long, I no no wanna :("
+            if ticks == 1163:
+                return f"Your ticks in real time is {second_to_minute(time)} (:heart:)"
             return f"Your ticks in real time is {second_to_minute(time)}"
         except:
             return f"'{ticks}' was not a valid tick count, please try again!"
