5adb2c9ae6b12e774bbe5fe7c69ffd281893103f
Valoix <97880029+Valoix@users.noreply.github.com>
2024-02-02 17:07:48 +1100

    Update responses.py
    [+] pretty colours in console :3 
    [+] Error Codes + Messages
    [+] A couple other things I cant remember lmao whoops

--- responses.py
+++ responses.py
@@ -11,18 +11,18 @@ def handle_response(user_data, message):
     if message == "cube++":
         for role in user_data.author.roles:
             if str(role) in big_boss_roles:
-                with open("C:\\Users\\Valoix\\Documents\\Programming Projects\\python\\PortalBot\\cube_count.txt", "r") as read_cube_count:
+                with open("./cube_count.txt", "r") as read_cube_count:
                     cube_count = int(read_cube_count.read())
-                with open("C:\\Users\\Valoix\\Documents\\Programming Projects\\python\\PortalBot\\cube_count.txt", "w") as write_cube_count:
+                with open("./cube_count.txt", "w") as write_cube_count:
                     write_cube_count.write(str(cube_count+1))
                     return f"So many cubes! I've now seen {str(cube_count+1)} cubes!"
     
     if message == "cube--":
         for role in user_data.author.roles:
             if str(role) in big_boss_roles:
-                with open("C:\\Users\\Valoix\\Documents\\Programming Projects\\python\\PortalBot\\cube_count.txt", "r") as read_cube_count:
+                with open("./cube_count.txt", "r") as read_cube_count:
                     cube_count = int(read_cube_count.read())
-                with open("C:\\Users\\Valoix\\Documents\\Programming Projects\\python\\PortalBot\\cube_count.txt", "w") as write_cube_count:
+                with open("./cube_count.txt", "w") as write_cube_count:
                     write_cube_count.write(str(cube_count-1))
                     return f"I was mistaken, cube rescinded. I've now seen only {str(cube_count-1)} cubes!"
 
@@ -36,16 +36,28 @@ def handle_response(user_data, message):
 
 
 def image_dox_blox():
-    pass
-    # REMOVED FOR SECURITY PURPOSES
+    myconfig = r"--psm 11 --osm 3"
+    text = pytesseract.image_to_string(Image.open("./downloads/image.jpg"))
+    print(text)
+    for term in secret.get_dox_terms():
+        if term in text:
+            return True, text
+        else:
+            return False, text
     
 def text_dox_blox(message):
-    pass
-    # REMOVED FOR SECURITY PURPOSES
+    for term in secret.get_dox_terms():
+        if term in message:
+            return True
+        else:
+            return False
         
 def name_dox_blox(username):
-    pass
-    # REMOVED FOR SECURITY PURPOSES
+    for term in secret.get_dox_terms():
+        if term in username:
+            return True
+        else:
+            return False
 
 def wr_db_add(wr_category, wr_time, wr_name, wr_date, wr_message_link):
     print("Begin Writing to WR DB")
@@ -73,35 +85,65 @@ def wr_db_add(wr_category, wr_time, wr_name, wr_date, wr_message_link):
         wr_category = "o"
     print("Translated")
 
-    with open("C:\\Users\\Valoix\\Documents\\Programming Projects\\python\\PortalBot\\wr_archive.txt", "a") as file:
+    with open("./wr_archive.txt", "a") as file:
         file.write(f"{wr_name} {wr_category} {wr_time} {wr_date} {wr_message_link}\n")
         print("Write Success")
     file.close()
+
+def process_wr_category(category):
+    # Category Handling
+    categories = {
+    "g": "Glitchless",
+    "nl": "NoSLA Legacy",
+    "nu": "NoSLA Unrestricted",
+    "i": "Inbounds", 
+    "o": "Out of Bounds"
+    }
+
+    return categories.get(category)
 
 def process_wr_output(raw_wr_search):
     #line_words = ["name", "category", "time", "date", "message link"]
 
     line_words = str(raw_wr_search).split(" ")
 
+    # Parses line_words
     wr_name = str(line_words[0]).lower()
-    wr_category = str(line_words[1]).lower()
+    wr_category = process_wr_category(line_words[1].lower())
     wr_time = str(line_words[2])
     wr_date = str(line_words[3])
     wr_message_link = str(line_words[4])
-
-    # Category Processing
-    if wr_category == "g":
-        wr_category = "Glitchless"
-    elif wr_category == "nl":
-        wr_category = "NoSLA Legacy"
-    elif wr_category == "nu":
-        wr_category = "NoSLA Unrestricted"
-    elif wr_category == "i":
-        wr_category = "Inbounds"
-    elif wr_category == "o":
-        wr_category = "Out of Bounds"
 
     return f"[{wr_date}] {wr_name.capitalize()} {wr_category} {wr_time} {wr_message_link}"
 
 def generate_message_link(server_id, channel_id, message_id):
     return f"https://discord.com/channels/{server_id}/{channel_id}/{message_id}"
+
+def print_colour(colour, text):
+
+    def message_red(text):
+        return "\033[31m{}\033[0m".format(text)
+
+    def message_green(text):
+        return "\033[32m{}\033[0m".format(text)
+
+    def message_blue(text):
+        return "\033[34m{}\033[0m".format(text)
+    
+    colour_functions = {
+        "R": message_red,
+        "G": message_green,
+        "B": message_blue,
+    }
+    return colour_functions.get(colour)(text)
+
+def print_error(code):
+    error_codes_dict = {
+        "000": "Error 000: Unclassified Error",
+        "201": "Error 201: ESC sequence found in message.",
+        "301": "Error 301: Dox Detected in message.",
+        "302": "Error 302: Dox Detected in name.",
+        "303": "Error 303: Dox Detected in image.",
+        "304": "Error 304: Ping Detected in message.",
+    }
+    return print_colour("R", error_codes_dict.get(code))
